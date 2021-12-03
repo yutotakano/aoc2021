08e1487cccadf659d28ae9a7da7159c39f11d80f
Yuto Takano <moa17stock@gmail.com>
2021-12-03 22:10:48 +0000

    Complete day 2 part 2 in OCaml

--- src/day2.ml
+++ src/day2.ml
@@ -1,23 +1,52 @@
+open Base
 open AOC
-
-type instruction = Move of int * int
 
 let result (a, b) = a * b
 
-let parseInstructions input =
-  let [instr; amount] = String.split_on_char ' ' input in
+let (|-) x f = f x; x
+
+type instruction1 = Move of int * int
+
+let parse_instructions1 input =
+  let [instr; amount] = String.split_on_chars ~on:[' '] input in
   match instr with
-    | "up" -> Move (0, (-1) * int_of_string amount)
-    | "down" -> Move (0, int_of_string amount)
-    | "forward" -> Move (int_of_string amount, 0)
-    | _ -> Move (0, 0)
+  | "up"      -> Move (0, (-1) * Int.of_string amount)
+  | "down"    -> Move (0, Int.of_string amount)
+  | "forward" -> Move (Int.of_string amount, 0)
+  | _         -> Move (0, 0)
 
-let rec exec = function
-| [] -> (0, 0)
-| (Move (a, b) :: xs) -> let (x, y) = exec xs in (x + a, y + b)
+let rec exec1 = function
+| []                  -> (0, 0)
+| (Move (a, b) :: xs) -> let (x, y) = exec1 xs in (x + a, y + b)
 
-let part1 input = string_of_int (result (exec (List.map parseInstructions input)))
+let part1 input =
+  input
+  |> List.map ~f:parse_instructions1
+  |> exec1
+  |> result
+  |> Int.to_string
 
-let part2 input = assert false
+type instruction2
+  = Forward of int
+  | ShiftAim of int
+
+let parse_instructions2 input =
+  let [instr; amount] = String.split_on_chars ~on:[' '] input in
+  match instr with
+  | "up"      -> ShiftAim ((-1) * Int.of_string amount)
+  | "down"    -> ShiftAim (Int.of_string amount)
+  | "forward" -> Forward (Int.of_string amount)
+
+let rec exec2 (x, y) aim = function
+| []                 -> (x, y)
+| (Forward n :: xs)  -> exec2 (x + n, y + (aim * n)) aim xs
+| (ShiftAim n :: xs) -> exec2 (x, y) (aim + n) xs
+
+let part2 input =
+  input
+  |> List.map ~f:parse_instructions2
+  |> exec2 (0, 0) 0
+  |> result
+  |> Int.to_string
 
 let () = aoc "day2" part1 part2
