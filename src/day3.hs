2c4257e6249628f72263a49dcbadd4674561b3b3
Yuto Takano <moa17stock@gmail.com>
2021-12-04 00:07:02 +0000

    Type annotate day 3 solution

--- src/day3.hs
+++ src/day3.hs
@@ -4,16 +4,18 @@ import Debug.Trace
 
 main = aoc "day3" part1 part2
 
-binToDec :: [Int] -> Int
+type Binary = [Int]
+
+binToDec :: Binary -> Int
 binToDec input = binToDec' 0 $ reverse input
   where
     binToDec' power [] = 0
     binToDec' power (lsb:revInput) = 2^power * lsb + binToDec' (power + 1) revInput
 
-flipBin :: [Int] -> [Int]
+flipBin :: Binary -> Binary
 flipBin = map (flip mod 2 . (+) 1)
 
-sumEachIndex :: [[Int]] -> [Int]
+sumEachIndex :: [Binary] -> Binary
 sumEachIndex = foldr1 (\x y -> map (uncurry (+)) $ zip x y)
 
 oneAboveHalf :: Int -> Int -> Int
@@ -22,22 +24,28 @@ oneAboveHalf len x
     | even len && x >= (len `div` 2) = 1
     | otherwise = 0
 
+parse :: [String] -> [Binary]
+parse input = map (map digitToInt) input
 
-san input = map (map digitToInt) input
+gamma :: [Binary] -> Binary
 gamma input = map (oneAboveHalf (length input)) . sumEachIndex $ input
+
+epsilon :: [Binary] -> Binary
 epsilon = flipBin . gamma
 
-part1 input = binToDec (gamma $ san input) * binToDec (epsilon $ san input)
+part1 :: [String] -> Int
+part1 input = binToDec (gamma $ parse input) * binToDec (epsilon $ parse input)
 
-filterMatches :: (Show a, Eq a) => ([[a]] -> [a]) -> [[a]] -> [a]
+filterMatches :: ([Binary] -> Binary) -> [Binary] -> Binary
 filterMatches _ [x] = x
-filterMatches f input = x : (filterMatches f . map tail $ filtered)
+filterMatches f input = msb : (filterMatches f . map tail $ filtered)
   where
-    (x:xs) = f input
-    filtered = filter ((== x) . head) $ input
+    msb = head $ f input
+    filtered = filter ((== msb) . head) $ input
 
+part2 :: [String] -> Int
 part2 input = binToDec oxy * binToDec co2
   where
-    oxy = filterMatches gamma $ san input
-    co2 = filterMatches epsilon $ san input
+    oxy = filterMatches gamma $ parse input
+    co2 = filterMatches epsilon $ parse input
 
